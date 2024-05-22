019badeafecebe5b0d7a716e36cac840d05fd825
Mavis <mavisgundaya@gmail.com>
2024-05-22 13:43:52 +0800

    project final

---
+++ frontend/adoption/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+import requests
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, 'home/index.html')
+
+def shop(request):
+
+    return render(request, 'home/shop.html')
